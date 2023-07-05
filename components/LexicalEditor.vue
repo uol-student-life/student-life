3d92dff302b4942f535a748cbae89b7148da7283
e.baidakova <lena.baydakova@gmail.com>
2023-07-05 16:25:38 +0400

    feat(journal): markdown support
    Resolves #17
    
    - Added markdown support to editor.

--- components/LexicalEditor.vue
+++ components/LexicalEditor.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { $getRoot, $getSelection } from "lexical";
+import theme from "./LexicalEditor/theme";
 import { ref } from "vue";
 
 import {
@@ -9,7 +10,6 @@ import {
   LexicalHistoryPlugin,
   LexicalOnChangePlugin,
   LexicalRichTextPlugin,
-  LexicalAutoLinkPlugin,
   LexicalLinkPlugin,
   LexicalListPlugin,
 } from "lexical-vue";
@@ -17,12 +17,15 @@ import {
 import { AutoLinkNode, LinkNode } from "@lexical/link";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { ListItemNode, ListNode } from "@lexical/list";
+import { CodeHighlightNode, CodeNode } from "@lexical/code";
+
+import AutoLinkPlugin from "./LexicalEditor/AutoLinkPlugin.vue";
+import MarkdownShortcutPlugin from "./LexicalEditor/MarkdownShortcutPlugin.vue";
+import CodeHighlightPlugin from "./LexicalEditor/CodeHighlightPlugin.vue";
 
 const config = {
   editable: true,
-  theme: {
-    // Theme styling goes here
-  },
+  theme,
   nodes: [
     HeadingNode,
     ListNode,
@@ -30,6 +33,8 @@ const config = {
     QuoteNode,
     AutoLinkNode,
     LinkNode,
+    CodeNode,
+    CodeHighlightNode,
   ],
   onError(error) {
     console.error(error);
@@ -47,23 +52,6 @@ function onChange(editorState) {
     console.log(root, selection);
   });
 }
-
-const URL_MATCHER =
-  /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
-
-const MATCHERS = [
-  (text) => {
-    const match = URL_MATCHER.exec(text);
-    return (
-      match && {
-        index: match.index,
-        length: match[0].length,
-        text: match[0],
-        url: match[0],
-      }
-    );
-  },
-];
 
 // Two-way binding
 const content = ref("");
@@ -89,9 +77,215 @@ const content = ref("");
         <LexicalHistoryPlugin />
         <LexicalAutoFocusPlugin />
         <LexicalLinkPlugin />
-        <LexicalAutoLinkPlugin :matchers="MATCHERS" />
+        <AutoLinkPlugin />
+        <MarkdownShortcutPlugin />
+        <CodeHighlightPlugin />
         <LexicalListPlugin />
       </LexicalComposer>
     </div>
   </div>
 </template>
+
+<!--styles mostly taken from https://github.com/wobsoriano/lexical-vue/tree/main/playground-->
+<style>
+.editor-container {
+  margin: 20px auto 20px auto;
+  border-radius: 2px;
+  max-width: 600px;
+  color: #000;
+  position: relative;
+  line-height: 20px;
+  font-weight: 400;
+  text-align: left;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.editor-inner {
+  background: #fff;
+  position: relative;
+}
+
+.editor-input {
+  min-height: 150px;
+  resize: none;
+  font-size: 15px;
+  position: relative;
+  tab-size: 1;
+  outline: 0;
+  padding: 15px 10px;
+  caret-color: #444;
+}
+
+.editor-placeholder {
+  color: #999;
+  overflow: hidden;
+  position: absolute;
+  text-overflow: ellipsis;
+  top: 15px;
+  left: 10px;
+  font-size: 15px;
+  user-select: none;
+  display: inline-block;
+  pointer-events: none;
+}
+
+.editor-text-bold {
+  font-weight: bold;
+}
+
+.editor-text-italic {
+  font-style: italic;
+}
+
+.editor-text-underline {
+  text-decoration: underline;
+}
+
+.editor-text-strikethrough {
+  text-decoration: line-through;
+}
+
+.editor-text-underlineStrikethrough {
+  text-decoration: underline line-through;
+}
+
+.editor-text-code {
+  background-color: rgb(240, 242, 245);
+  padding: 1px 0.25rem;
+  font-family: Menlo, Consolas, Monaco, monospace;
+  font-size: 94%;
+}
+
+.editor-text-hashtag {
+  background-color: rgba(88, 144, 255, 0.15);
+  border-bottom: 1px solid rgba(88, 144, 255, 0.3);
+}
+
+.editor-link {
+  color: rgb(33, 111, 219);
+  text-decoration: none;
+}
+
+.editor-code {
+  @apply bg-neutral-100;
+  font-family: Menlo, Consolas, Monaco, monospace;
+  display: block;
+  padding: 8px 8px 8px 52px;
+  line-height: 1.53;
+  font-size: 13px;
+  margin: 0;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  tab-size: 2;
+  /* white-space: pre; */
+  overflow-x: auto;
+  position: relative;
+}
+
+.editor-code:before {
+  content: attr(data-gutter);
+  position: absolute;
+  background-color: #eee;
+  left: 0;
+  top: 0;
+  border-right: 1px solid #ccc;
+  padding: 8px;
+  color: #777;
+  white-space: pre-wrap;
+  text-align: right;
+  min-width: 25px;
+}
+.editor-code:after {
+  content: attr(data-highlight-language);
+  top: 0;
+  right: 3px;
+  padding: 3px;
+  font-size: 10px;
+  text-transform: uppercase;
+  position: absolute;
+  color: rgba(0, 0, 0, 0.5);
+}
+
+.editor-tokenComment {
+  color: slategray;
+}
+
+.editor-tokenPunctuation {
+  color: #999;
+}
+
+.editor-tokenProperty {
+  color: #905;
+}
+
+.editor-tokenSelector {
+  color: #690;
+}
+
+.editor-tokenOperator {
+  color: #9a6e3a;
+}
+
+.editor-tokenAttr {
+  color: #07a;
+}
+
+.editor-tokenVariable {
+  color: #e90;
+}
+
+.editor-tokenFunction {
+  color: #dd4a68;
+}
+
+.editor-paragraph {
+  @apply mb-2 text-stone-600;
+  position: relative;
+}
+
+.editor-heading-h1 {
+  @apply mb-4 text-xl font-bold text-stone-600;
+}
+
+.editor-heading-h2 {
+  @apply mb-4 mt-4 border-b border-b-neutral-200 pb-4 text-lg font-medium text-stone-600;
+}
+
+.editor-quote {
+  margin-left: 20px;
+  font-size: 15px;
+  color: rgb(101, 103, 107);
+  border-left-color: rgb(206, 208, 212);
+  border-left-width: 4px;
+  border-left-style: solid;
+  padding-left: 16px;
+}
+
+.editor-list-ol {
+  margin-left: 16px;
+  list-style: revert;
+}
+
+.editor-list-ul {
+  margin-left: 16px;
+  list-style: unset;
+}
+
+.editor-listitem {
+  margin: 8px 32px 8px 32px;
+}
+
+.editor-nested-listitem {
+  list-style-type: none;
+}
+
+pre::-webkit-scrollbar {
+  background: transparent;
+  width: 10px;
+}
+
+pre::-webkit-scrollbar-thumb {
+  background: #999;
+}
+</style>
---
+++ components/LexicalEditor/MarkdownShortcutPlugin.vue
@@ -0,0 +1,20 @@
+<!-- Taken from https://github.com/wobsoriano/lexical-vue/tree/main/playground-->
+<script lang="ts" setup>
+import { LexicalMarkdownShortcutPlugin } from "lexical-vue";
+import type { Transformer } from "@lexical/markdown";
+import {
+  ELEMENT_TRANSFORMERS,
+  TEXT_FORMAT_TRANSFORMERS,
+  TEXT_MATCH_TRANSFORMERS,
+} from "@lexical/markdown";
+
+const PLAYGROUND_TRANSFORMERS: Transformer[] = [
+  ...ELEMENT_TRANSFORMERS,
+  ...TEXT_FORMAT_TRANSFORMERS,
+  ...TEXT_MATCH_TRANSFORMERS,
+];
+</script>
+
+<template>
+  <LexicalMarkdownShortcutPlugin :transformers="PLAYGROUND_TRANSFORMERS" />
+</template>
