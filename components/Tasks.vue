16b1d73e7f9e180ea4edcc2b0068b169be829b30
Mayra Torres <81662672+mayramtv@users.noreply.github.com>
2023-07-21 17:30:07 -0400

    Accordion done, only one milestone open at a time. Add basic check list

--- components/Tasks.vue
+++ components/Tasks.vue
@@ -1,3 +1,79 @@
 <template>
-  <Section title="Tasks"> </Section>
+  <Section title="Tasks"> 
+    <UAccordion :items="list" :ui="{ wrapper: 'flex flex-col w-full' }" size="xl">
+
+      <template #default="{ item, index, open }">
+        <UButton variant="ghost" class="border-b text-stone-400 border-gray-200 dark:border-gray-700 hover:bg-stone-200" :ui="{ rounded :'rounded-none', padding: { sm:'p-3' } }">
+          <!-- Chevron template -->
+          <template #leading>
+            <UIcon
+              name="i-heroicons-chevron-right-20-solid"
+              class="w-5 h-5 p-4 transform transition-transform duration-200"
+              :class="[open && 'rotate-90']"
+            />
+          </template>
+          <!-- Milestone name -->
+          <template #trailing p-4> 
+            <span class="truncate">{{ index + 1 }}. {{ item.mile_title }}</span>
+          </template>
+        </UButton>
+      </template>
+
+      <!-- Task list -->
+      <template #item="{ item }">
+        <div v-for="tsk in item.tasks" class="in-line">
+          <div class="p-3 border-b mx-6" >
+            <UCheckbox color="blue" v-bind:label="tsk"/>
+          </div>
+        </div>
+      </template>
+
+    </UAccordion>
+  </Section>
 </template>
+
+<!-- ---------------------------------- -->
+<script setup>
+import { _hidden } from '#tailwind-config/theme/aria';
+
+// Sample list
+const m_list = [
+  {
+    mile_title: "First Milestone 1",
+    tasks: ["Item 1", "Item 2"]
+  },
+  {
+    mile_title: "Second Milestone 2",
+    tasks: ["Item 1", "Item 2"]
+  },
+  {
+    mile_title: "Third Milestone 3",
+    tasks: ["Item 1", "Item 2", "Item 2"]
+  },
+  {
+    mile_title: "Fourth Milestone 4",
+    tasks: ["Item 1", "Item 2"]
+  },
+  {
+    mile_title: "Fifth Milestone 5",
+    tasks: ["Item 1", "Item 2"]
+  },
+  {
+    mile_title: "Sixth Milestone 6",
+    tasks: ["Item 1", "Item 2", "Item 2"]
+  }
+]
+//  Sample list as ref
+const list = ref(m_list.map(item =>{
+  return { ...item, open: false }
+}))
+
+// -----Code to use with props----- 
+// const props = defineProps({
+//   milestones:{
+//     Array,
+//   }
+// });
+// const list = props.milestones;
+
+</script>
