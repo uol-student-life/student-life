690589f191ec632fd3551542a21bc5ab8f4e3e15
Mayra Torres <81662672+mayramtv@users.noreply.github.com>
2023-07-25 00:52:26 -0400

    feat(ui: button with popover): Added to tasks button popover with inp… (#71)
    * feat(ui: button with popover): Added to tasks button popover with input form
    
    * feat(ui: button with popover) resolve conflicts and add sections

--- components/Milestones.vue
+++ components/Milestones.vue
@@ -1,38 +1,74 @@
 <template>
   <Section title="Milestones">
-    <!-- Panel slider -->
-    <div>
-      <UButton class="ml-6" label="Milestone" @click="isOpen = true" />
-      <USlideover v-model="isOpen">
-        <UCard
-          class="flex flex-1 flex-col"
-          :ui="{
-            body: { base: 'flex-1' },
-            ring: '',
-            divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-          }"
-        >
-          <template #header>
-            <div class="flex items-center justify-between">
-              <div class="item-center flex">
-                <button class="pr-2" @click="isOpen = false">
-                  <ArrowLeftIcon
-                    class="max-w-4 h-4 text-stone-700 hover:text-stone-500"
-                  />
-                </button>
-                <span class="pl-2 font-semibold text-stone-500">Back</span>
-              </div>
-              <!-- milestone counter -->
-              <div class="font-semibold text-stone-500">0</div>
+
+    <!-- Adds the popover button to add new milestones  -->
+    <template #suffix>
+      <UPopover :popper="{ placement: 'bottom' }">
+        <UButton color="gray" icon="i-heroicons-plus-circle" variant="link" />
+        <template #panel="{ close }" >
+          <form >
+            <div class="align-center flex justify-between gap-4 p-6">
+              <UInput type='milestone' color="gray" placeholder="New Milestone"/>
+              <UButton color="gray" type="submit"> Add Milestone </UButton>
             </div>
-          </template>
-          <!-- milestone name -->
-          <div>milestone name goes here</div>
-          <!-- journal tagged with the milestone -->
-          <div>journal list goes here</div>
-        </UCard>
-      </USlideover>
-    </div>
+          </form>
+        </template>
+      </UPopover> 
+    </template>
+
+    <template class="grid min-h-full grid-rows-[minmax(0,_1fr)_minmax(0,_1fr)] overflow-hidden">
+      <section class="grid grid-rows-[auto_1fr] bg-stone-100 last:border-b-0">
+        <div class="flex items-center justify-between gap-2 p-2 px-6">
+          <h2 class="flex items-center text-md font-extrabold text-stone-400">
+            In Progress
+          </h2>
+          <h2 class="text-md font-extrabold text-stone-400">1</h2>
+        </div>
+
+        <!-- Panel slider -->
+        <div>
+          <UButton class="ml-6" label="Milestone" @click="isOpen = true" />
+          <USlideover v-model="isOpen">
+            <UCard
+              class="flex flex-1 flex-col"
+              :ui="{
+                body: { base: 'flex-1' },
+                ring: '',
+                divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+              }"
+            >
+              <template #header>
+                <div class="flex items-center justify-between">
+                  <div class="item-center flex">
+                    <button class="pr-2" @click="isOpen = false">
+                      <ArrowLeftIcon
+                        class="max-w-4 h-4 text-stone-700 hover:text-stone-500"
+                      />
+                    </button>
+                    <span class="pl-2 font-semibold text-stone-500">Back</span>
+                  </div>
+                  <!-- milestone counter -->
+                  <div class="font-semibold text-stone-500">0</div>
+                </div>
+              </template>
+              <!-- milestone name -->
+              <div>milestone name goes here</div>
+              <!-- journal tagged with the milestone -->
+              <div>journal list goes here</div>
+            </UCard>
+          </USlideover>
+        </div>
+      </section>
+      <section class="grid grid-rows-[auto_1fr]  bg-stone-100 last:border-b-0">
+        <div class="flex items-center justify-between gap-2 p-2 px-6">
+          <h2 class="flex items-center text-md font-extrabold text-stone-400">
+            Completed
+          </h2>
+          <h2 class="text-md font-extrabold text-stone-400">0</h2>
+        </div>
+      </section>
+    </template>
+
   </Section>
 </template>
 
--- components/Tasks.vue
+++ components/Tasks.vue
@@ -1,5 +1,26 @@
 <template>
   <Section title="Tasks"> 
+  
+    <!-- Adds the popover button to add new tasks  -->
+    <template #suffix>
+      <UPopover :popper="{ placement: 'bottom' }">
+        <UButton color="gray" icon="i-heroicons-plus-circle" variant="link" />
+        <template #panel="{ close }" >
+          <form >
+            <div class="grid grid-cols-1 align-center gap-4 p-6">
+              <USelect :options="[0, 1, 2, 3, 4, 5]" color="gray" variant="outline" placeholder="Select Milestone"/>
+              <UInput type='text' color="gray" placeholder="New Task"/>
+              <UInput type='datetime-local' color="gray" placeholder="Date" />
+              <div class="flex w-full justify-center">
+                <UButton color="blue" variant="outline" class="text-gray-50 bg-slate-400 justify-center p-3 w-2/3 hover:bg-slate-500" type="submit"> Add Task </UButton>
+              </div>
+            </div>
+          </form>
+        </template>
+      </UPopover> 
+    </template>
+
+    <!-- Accordion Component -->
     <UAccordion :items="list" :ui="{ wrapper: 'flex flex-col w-full' }" size="xl">
 
       <template #default="{ item, index, open }">
