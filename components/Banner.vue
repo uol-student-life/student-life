4e83587ee45f4b25992320115ece87ba81f3f5ea
Mayra Torres <81662672+mayramtv@users.noreply.github.com>
2023-08-09 16:28:31 -0400

    ui:banner Fix color contrast. Banneer displays images and quote based on journal day

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,18 +1,18 @@
 <template>
     <div  class="basis-[35%] items-center bg-center" v-bind:style="{ backgroundImage:'url(' + getImg()[0] + ')' }">
         <!-- Hero -->
-        <div class="bg-black/[.15] h-[15%] text-center">
+        <div class="backdrop-blur-2xl h-[15%] text-center">
             <Hero :date="currentJournal?.journalDate" />
         </div>
         <!-- Quote -->
         <div class="h-[70%] w-full flex items-center justify-center">
-            <div class="w-2/3 rounded-lg bg-black/[.20]">
-                <h2 class="m-2 text-lg text-center font-semibold text-slate-100">{{ getQuote()[1] }}</h2>
-                <p class="m-2 text-md text-right font-semibold text-slate-100">{{ getQuote()[0] }}</p>
+            <div class="w-2/3 rounded-lg backdrop-blur-2xl">
+                <h2 class="m-2 text-lg text-center font-semibold text-slate-100 drop-shadow-[2px_2px_4px_rgba(0,0,0)]">{{ getQuote()[1] }}</h2>
+                <p class="m-2 text-md text-right font-semibold text-slate-100 drop-shadow-[2px_2px_4px_rgba(0,0,0)] ">{{ getQuote()[0] }}</p>
             </div>
         </div>
         <!-- Information button -->
-        <div class="h-[15%] group relative flex float-right items-center p-1 hover:bg-black/[.5]">
+        <div class="h-[15%] group relative flex float-right items-center drop-shadow-[1px_1px_2px_rgba(0,0,0)] p-1 hover:bg-black/[.5]">
             <div class="invisible group-hover:visible text-white">
                 <p class="text-xs text-right">{{ getImg()[1] }}</p>
                 <p class="text-xs text-right">Quotes by <a href="https://www.brainyquote.com/">BrainyQuote</a></p>
@@ -27,25 +27,33 @@
 
 
 <script setup>
+
     import { _backgroundImage } from "#tailwind-config/theme";
     import bannerImages from "../static/images.json" 
     import bannerQuotes from "../static/quotes.json" 
 
-    const day = new Date().getDate();
+    const props = defineProps({
+        currentJournal: Object,
+    });
+
 
     // selects an image based on the day
     const getImg = () => {
-        return [bannerImages[day - 1]['link'], bannerImages[day - 1]['reference'], ]
+        return [
+            bannerImages[new Date(props.currentJournal.journalDate).getDate() - 1]['link'], 
+            bannerImages[new Date(props.currentJournal.journalDate).getDate() - 1]['reference'], 
+        ]
     }
 
     // it can be adjusted to select a quote based on mood
     // selects a quote based on the day 
     const getQuote = () => {
-        return [bannerQuotes[day - 1]['autor'], bannerQuotes[day - 1]['quote']]
+        return [
+            bannerQuotes[new Date(props.currentJournal.journalDate).getDate() - 1]['autor'], 
+            bannerQuotes[new Date(props.currentJournal.journalDate).getDate() - 1]['quote']
+        ]
     }
 
-    const props = defineProps({
-        currentJournal: Object,
-    });
+    
     
 </script>
