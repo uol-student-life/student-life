473dae4a812e31f17ab968429a5bafe19704e870
Sullyvan Seam <seam.sullyvan@gmail.com>
2023-08-10 20:35:47 +0700

    ui-moodchart-moodselector

---
+++ components/MoodSelector.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex justify-between">
+    <div class="text-center">
+      <button class="transition duration-150 hover:scale-150">
+        <MoodIconsMotivated class="h-6 w-6" />
+      </button>
+      <span class="text-xs text-stone-400">Motivated</span>
+    </div>
+
+    <div class="text-center">
+      <button class="transition duration-150 hover:scale-150">
+        <MoodIconsRelaxed class="h-6 w-6" />
+      </button>
+      <span class="text-xs text-stone-400">Relaxed</span>
+    </div>
+    <div class="text-center">
+      <button class="transition duration-150 hover:scale-150">
+        <MoodIconsStressed class="h-6 w-6" />
+      </button>
+      <span class="text-xs text-stone-400">Stressed</span>
+    </div>
+    <div class="text-center">
+      <button class="transition duration-150 hover:scale-150">
+        <MoodIconsDepressed class="h-6 w-6" />
+      </button>
+      <span class="text-xs text-stone-400">Depressed</span>
+    </div>
+  </div>
+</template>
