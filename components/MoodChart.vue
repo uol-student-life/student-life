6e7b41f619364dd0b58561587ee8f7107d60e158
Sullyvan <seam.sullyvan@gmail.com>
2023-08-11 10:57:37 +0700

    ui-moodchart-moodselector (#96)

---
+++ components/MoodChart.vue
@@ -0,0 +1,75 @@
+<script setup>
+Chart.defaults.color = "#78716c";
+
+import { Chart } from "chart.js/auto";
+const labels = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Mood",
+      //mood data goes here
+      data: [3, 2, 2, 1, 3, 1, 0],
+      borderColor: "#f59e0b",
+      fill: false,
+      tension: 0.5,
+      boxWidth: 25,
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        ticks: {
+          min: 0,
+          max: 3,
+          stepSize: 1,
+          //transform numerical data into strings on y-axis
+          callback: function (label) {
+            switch (label) {
+              case 0:
+                return "Depressed";
+              case 1:
+                return "Stressed";
+              case 2:
+                return "Relaxed";
+              case 3:
+                return "Motivated";
+            }
+          },
+        },
+      },
+    },
+
+    plugins: {
+      legend: {
+        //prevent data hiding when clicking on legend
+        onClick: (e) => e.stopPropagation(),
+        align: "end",
+        labels: {
+          boxWidth: 25,
+        },
+      },
+      tooltip: {
+        enabled: false,
+      },
+    },
+  },
+};
+
+onMounted(() => {
+  const myChart = new Chart(document.getElementById("myChart"), config);
+});
+</script>
+
+<template>
+  <div>
+    <canvas id="myChart" class="w-64"></canvas>
+  </div>
+</template>
--- components/MoodTracker.vue
+++ components/MoodTracker.vue
@@ -1,27 +1,15 @@
 <template>
   <Section title="Mood tracker">
-    <!-- Mood chart -->
     <div class="px-6">
-      <span>Mood Chart</span>
-    </div>
-    <!-- Mood icons -->
-    <div class="flex justify-between px-6 py-4">
-      <div class="text-center">
-        <MoodIconsMotivated class="h-6 w-6" />
-        <span class="text-xs text-stone-400">Motivated</span>
+      <!-- Mood chart -->
+      <div>
+        <MoodChart class="pb-4" />
       </div>
-      <div class="text-center">
-        <MoodIconsRelaxed class="h-6 w-6" />
-        <span class="text-xs text-stone-400">Relaxed</span>
+      <!-- Mood icons -->
+      <div class="text-center text-sm font-semibold text-stone-500">
+        How are you feeling today?
       </div>
-      <div class="text-center">
-        <MoodIconsStressed class="h-6 w-6" />
-        <span class="text-xs text-stone-400">Stressed</span>
-      </div>
-      <div class="text-center">
-        <MoodIconsDepressed class="h-6 w-6" />
-        <span class="text-xs text-stone-400">Depressed</span>
-      </div>
+      <MoodSelector class="pt-2" />
     </div>
   </Section>
 </template>
