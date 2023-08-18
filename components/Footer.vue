fde76c4eb790707d6f50bca51c8924361c46e0c5
e.baidakova <lena.baydakova@gmail.com>
2023-08-18 13:10:26 +0400

    feat: show amount of words and milestones in footer
    Resolves #109

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup>
+import pluralize from "pluralize";
+import { wordCount } from "../stores/wordCount";
+
+const props = defineProps({
+  milestones: Number,
+});
+</script>
+<template>
+  <footer
+    class="flex justify-end gap-8 border-t border-t-neutral-200 px-8 py-4 text-sm text-neutral-400"
+  >
+    <span v-if="wordCount">{{ pluralize("word", wordCount, true) }}</span>
+    <span v-if="milestones">{{
+      pluralize("milestone", milestones, true)
+    }}</span>
+  </footer>
+</template>
